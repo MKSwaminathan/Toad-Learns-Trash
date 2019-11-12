d60b45f932ec7a8f7638204ac247cc4728fdcdbf
Akash Shukla <akashukla@utexas.edu>
2019-11-11 19:41:21 -0600

    trash selector now downloads images, gets boxes, and writes to desired format - actually works now

--- gluon/trash_selector.py
+++ gluon/trash_selector.py
@@ -6,6 +6,8 @@ import os
 import sys
 import zipfile
 import mxnet as mx
+import scipy as sc
+import scipy.ndimage as ndimage
 
 
 def label_trash(png):
@@ -39,51 +41,59 @@ def get_box(labeled_img):
         roi2[i, xmin:xmax, ymin-2:ymin+2] = 1
         roi2[i, xmin:xmax, ymax-2:ymax+2] = 1
         i += 1
-        box = [xmin, ymin, xmax, ymax]
+        box = np.array([xmin, ymin, xmax, ymax])
     # roi2, roi, labels[1:]
-    return box, img.shape[0], labeled_img.shape[1], labels[1:]
+    #print(labels[1:].shape)
+    return box, labeled_img.shape[0], labeled_img.shape[1], labels[1:]
+
 
 
 def get_trash():
     outpath = os.path.abspath('')+'/trash_images'
-    os.mkdir(outpath)
-    urls = np.genfromtxt('trash_urls.txt', delimiter='\n', dtype='str')
+    # os.mkdir(outpath)
+    urls = np.genfromtxt('trash_urls.txt', delimiter='\n', dtype='str')[:-1]
+    urls=[url for url in urls if 'val' not in url]
+    #raw = urls[0:len(urls):2]
+    raw = [url for url in urls if 'seg' not in url]
+    #seg = urls[1:len(urls):2]
+    seg = [url for url in urls if 'seg' in url]
     for url in urls:
         utils.download(url, path=outpath)
-    lpath = os.path.abspath('')+'/trash_labels'
-    os.mkdir(lpath)
+    #lpath = os.path.abspath('')+'/trash_labels'
+    # os.mkdir(lpath)
     #trash_boxes = np.zeros((len(urls), 4), dtype='str')
     with open('val.lst', 'w') as fw:
-        for i in range(len(urls)):
-            img = plt.imread(outpath+'/'+urls[i])
-            img = label_trash(img)
+        for i in range(len(raw)):
+            img = plt.imread(outpath+'/'+raw[i][-22:])
+            l_img = plt.imread(outpath+'/'+seg[i][-26:])
+            l_img = label_trash(l_img)
             #trash_boxes[i, :], w, h, ids = get_box(img)
-            trash_box[i, :], w, h, ids = get_box(img)
+            trash_box, w, h, ids = get_box(l_img)
             A, B, C, D = 4, 5, w, h
             #trash_boxes[i, 1:-1:2] /= flot(w)
             #trash_boxes[i, 2:-1:2] /= float(h)
-            trash_box[1:-1:2] /= flot(w)
-            trash_box[2:-1:2] /= float(h)
-            labels = np.hstack((ids.reshape(-1, 1), trash_box)).astype('float')
+            trash_box[0:len(trash_box):2] = trash_box[1:-1:2]/w
+            trash_box[1:len(trash_box):2] = trash_box[1:-1:2]/h
+            labels = np.hstack((ids, trash_box)).astype('float')
             str_idx = [str(i)]
             str_header = [str(x) for x in [A, B, C, D]]
             str_labels = [str(x) for x in labels]
-            str_path = [outpath+'/'+urls[i]]
-            line = '\t'.join(str_idx + str_header + str_labels + str_path) + '\n'
+            str_path = [outpath+'/'+raw[i]]
+            line = '\t'.join(str_idx + str_header +
+                             str_labels + str_path) + '\n'
             fw.write(line)
-
     #np.save(lpath+'/trash_boxes', trash_boxes)
-    return trash_boxes
+    #return trash_boxes
 
 
-#with open('val.lst', 'w') as fw:
+# with open('val.lst', 'w') as fw:
 #    for i in range(4):
 #        line = write_line('dog.jpg', img.shape, all_boxes, all_ids, i)
 #        print(line)
 #        fw.write(line)
 #
 #
-#def write_line(img_path, im_shape, boxes, ids, idx):
+# def write_line(img_path, im_shape, boxes, ids, idx):
 #    h, w, c = im_shape
 #    # for header, we use minimal length 2, plus width and height
 #    # with A: 4, B: 5, C: width, D: height
