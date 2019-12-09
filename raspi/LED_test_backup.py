43736fa3a8488635693f855a739e7ff95f38a41a
Meenakshi Swaminathan <shwetha@meenakshis-mbp.home>
2019-12-08 20:13:44 -0600

    copying code on raspi to main repo here

---
+++ raspi/LED_test_backup.py
@@ -0,0 +1,25 @@
+import time
+import RPi.GPIO as io
+
+
+def init_port(port):
+    io.setmode(io.BCM)
+    io.setup(port,io.OUT)
+    io.output(port,0)
+
+n_ports = 20
+for p in range(n_ports):
+    print('init port: ',p)
+    init_port(p)
+
+while True:
+    for p in range(n_ports):
+        print('running port: ',p)
+        io.output(p,1)
+        time.sleep(0.5)
+        io.output(p,0)
+        time.sleep(0.5) 
+        #io.output(p,1)
+        #time.sleep(0.5)
+        #io.output(p,0)
+        #time.sleep(0.5) 
---
+++ raspi/TCP_Realsense_backup.py
@@ -0,0 +1,35 @@
+import socket
+import time
+import RPi.GPIO as io
+
+
+def init_port():
+    io.setmode(io.BCM)
+    io.setup(4,io.OUT)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = 'Meenakshis-MacBook-Pro.local'
+port = 1111
+
+print("Connecting")
+s.connect((host, port))
+
+init_port()
+
+while True:
+
+    s_msg = s.recv(1024).decode()
+    print(s_msg)
+    if s_msg == 'blink':
+        for i in range (0,5):
+            io.output(4,0)
+            time.sleep(0.30)
+            io.output(4,1)
+            time.sleep(0.30)
+    
+    msg = 'Tell me to blink'
+    s.send(msg.encode())
+    #s.close()
+    
+    
